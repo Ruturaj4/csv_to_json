9f59e8084d35986bdd44ebd68ff5db12d3dcf947
Ruturaj <ruturajkvaidya@ku.edu>
2018-12-19 23:00:39 -0600

    fixes

--- csv_to_json/csv_to_json.py
+++ csv_to_json/csv_to_json.py
@@ -8,12 +8,10 @@ import sys
 def convert(csv):
     try: 
         r = pd.read_csv(csv)
-        r.to_json(fileName+".json", orient='records')
+        r.to_json(csv+".json", orient='records')
         # Print it as a json
         # It is that simple!
     except FileNotFoundError:
         print("No such file!")
-        sys.exit()
     except:
-        print("Usage: csv_to_json.main(filename)")
-        sys.exit()
+        print("Usage: csv_to_json.convert(filename)")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csv_to_json",
-    version="0.0.3",
+    version="0.0.4",
     author="ruturaj4",
     author_email="ruturajkvaidya@gmail.com",
     description="A simple csv to json coverter",
